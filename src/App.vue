8eb4aafc593a635b5d3011a365eb69fc896555d2
pingshian <pingshian0131@gmail.com>
2023-12-31 14:32:40 +0800

    [add] first-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ProductPage from "./components/ProductPage.vue";
+import ProductDetail from "@/components/ProductDetail.vue";
+</script>
+
+<template>
+    <div class="container">
+    <div class="row py-3">
+      <div class="col-md-6">
+        <ProductPage />
+      </div>
+      <div class="col-md-6">
+        <ProductDetail />
+      </div>
+    </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
